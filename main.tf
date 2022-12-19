977a04e698bfe831f0dbfb919cfc4a5f881d9086
similly <similly@gmail.com>
2022-12-19 11:35:16 +0000

    Updated provider, formatted variables

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region  = var.region
-  profile = "default"
 }
 
 module "security_group_http" {
--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "region" {
-    description = "Region"
+  description = "Region"
 }
 variable "cidr_blocks" {
-    description = "APIPA CIDR Blocks"
-    type = list(string)
-    default = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/24"]
+  description = "APIPA CIDR Blocks"
+  type        = list(string)
+  default     = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/24"]
 }
