386ef9ff289bf9ff581d78e3efd25136ce781c68
similly <similly@gmail.com>
2022-12-19 17:20:52 +0000

    Formatted backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "srmlab-terraform-state"
-        key = "terraform-module-securitygroup/terraform.state"
-        region = "us-east-1"
-        dynamodb_table = "srmlab-terraform-state-locks"
-        encrypt = true
-    }
+  backend "s3" {
+    bucket         = "srmlab-terraform-state"
+    key            = "terraform-module-securitygroup/terraform.state"
+    region         = "us-east-1"
+    dynamodb_table = "srmlab-terraform-state-locks"
+    encrypt        = true
+  }
 }
