770aba6d84027b5d4ca99209140a3cc1bd3af00f
similly <similly@gmail.com>
2022-12-19 16:50:13 +0000

    Fix key

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "srmlab-terraform-state"
-        key = "/terraform-module-securitygroup/terraform.state"
+        key = "terraform-module-securitygroup/terraform.state"
         region = "us-east-1"
         dynamodb_table = "srmlab-terraform-state-locks"
         encrypt = true
